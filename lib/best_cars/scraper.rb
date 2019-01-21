6f71ab962090bbac796b473f6f73feacb3e84299
Marie Burns <mcburns2222@gmail.com>
2019-01-21 23:59:19 +0000

    begin scraper method scrape_cars

--- lib/best_cars/scraper.rb
+++ lib/best_cars/scraper.rb
@@ -1,7 +1,19 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 class BestCars::Scraper
   
   #add 2 scraper methods
-  def scrape_cars
+  def self.scrape_cars
+    cars = []
+    doc = Nokogiri::HTML(open("https://www.fueleconomy.gov/feg/best-worst.shtml"))
+    binding.pry
+    doc.css('tbody')
+    
+    # car name doc.css('tbody').first.css('div.desc a').text 
+    # epa class doc.css('tbody').first.css('th.vclass').text
+    # mpg doc.css('tbody').first.css('td.auto-mpg').text
   # 1 first level to get car name, type of engine, gas mileage
   # collect array of hashes. each hash is a car.  the hash will be used to create instances of the car class
   end
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,11 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 require_relative "best_cars/version.rb"
 require_relative "best_cars/cli.rb"
+require_relative "best_cars/scraper.rb"
+require_relative "best_cars/cars.rb"
 
 
 
