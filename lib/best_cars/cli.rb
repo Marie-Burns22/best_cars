1d0b87e54326652b61fca73b3f80176b36f974ff
Marie Burns <mcburns2222@gmail.com>
2019-01-22 07:11:19 +0000

    Begin code based on user input

--- lib/best_cars/cli.rb
+++ lib/best_cars/cli.rb
@@ -3,7 +3,7 @@ class BestCars::CLI
   def start 
     puts "Here are the top 10 fuel-efficient cars."
     BestCars::Scraper.scrape_cars
-    
+    puts "Choose the number car you want more information about."
     list_cars
     
     get_input
@@ -16,10 +16,14 @@ class BestCars::CLI
   end
   
   def get_input
-    puts "Choose the number car you want more information about."
     input = gets.strip
-    if input.to_i.between?(1, 10)
-      #either puts all the attributes or use url to call second scrape method
+    index = input.to_i - 1
+    if index.between?(0, 9)
+      selected_car =  BestCars::Car.all[index]
+      puts "The mpg is #{selected_car.mpg}."
+     # puts the other attributes from the page.
+      #  ask user if they would like to know about the range and fuel cost or look up another car.
+      # if yes, call second scraper method passing the car url. If no, ask for car number and call get_input.
     else
       puts "Invalid choice. Choose a number between 1 and 10."
       get_input
