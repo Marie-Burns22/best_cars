6ad9ee84163c67a760fc3a495472e735b813f252
Marie Burns <mcburns2222@gmail.com>
2019-01-22 06:49:19 +0000

    Add get input method to cli

--- lib/best_cars/cli.rb
+++ lib/best_cars/cli.rb
@@ -3,6 +3,26 @@ class BestCars::CLI
   def start 
     puts "Here are the top 10 fuel-efficient cars."
     BestCars::Scraper.scrape_cars
+    
+    list_cars
+    
+    get_input
   end
   
+  def list_cars
+    BestCars::Car.all.each.with_index(1) do |car, index|
+      puts "#{index}. #{car.model}"
+    end
+  end
+  
+  def get_input
+    puts "Choose the number car you want more information about."
+    input = gets.strip
+    if input.to_i.between?(1, 10)
+      #either puts all the attributes or use url to call second scrape method
+    else
+      puts "Invalid choice. Choose a number between 1 and 10."
+      get_input
+    end
+  end
 end  
--- lib/best_cars/scraper.rb
+++ lib/best_cars/scraper.rb
@@ -4,7 +4,6 @@ require 'pry'
 
 class BestCars::Scraper
   
-  #add 2 scraper methods
   def self.scrape_cars
     cars = []
     doc = Nokogiri::HTML(open("https://www.fueleconomy.gov/feg/best-worst.shtml"))
@@ -12,29 +11,21 @@ class BestCars::Scraper
     car_array = doc.css('table#bestcars').css('tbody')
     
     car_array.each do |car_row|
-    attributes = {
-      model:  car_row.css('div.desc a').text,
-      epa_class: car_row.css('th.vclass').text,
-      mpg: car_row.css('td.auto-mpg').text,
-      fuel_type: car_row.css('button').children.text,
-      url:
-    }
+      car_attributes = {
+        model: car_row.css('div.desc a').text,
+        epa_class: car_row.css('th.vclass').text,
+        mpg: car_row.css('td.auto-mpg').text,
+        fuel_type: car_row.css('button').children.text,
+        url: car_row.css('div.desc a')[0].attributes['href'].value
+      }
     
-    Car.new(attributes)
-    
-    
-    binding.pry
-    end
-    
-  # 1 first level to get car name, type of engine, gas mileage
-  # collect array of hashes. each hash is a car.  the hash will be used to create instances of the car class
+      car = BestCars::Car.new(car_attributes)
+      end
   end
   
   def scrape_car_info(car_URL)
   # 1 second level to get range and emissions and other data
   # called based on user input. takes an argument of the car URL.
   end
-  
-  
   
 end
