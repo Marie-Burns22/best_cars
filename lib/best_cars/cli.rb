ccdcc90fb2073ad6ac8e5e9ef758d471ddb52831
Marie Burns <mcburns2222@gmail.com>
2019-01-22 00:52:31 +0000

    Scrape car attributes

--- lib/best_cars/cli.rb
+++ lib/best_cars/cli.rb
@@ -2,6 +2,7 @@ class BestCars::CLI
   
   def start 
     puts "Here are the top 10 fuel-efficient cars."
+    BestCars::Scraper.scrape_cars
   end
   
 end  
--- lib/best_cars/scraper.rb
+++ lib/best_cars/scraper.rb
@@ -8,12 +8,22 @@ class BestCars::Scraper
   def self.scrape_cars
     cars = []
     doc = Nokogiri::HTML(open("https://www.fueleconomy.gov/feg/best-worst.shtml"))
-    binding.pry
-    doc.css('tbody')
     
-    # car name doc.css('tbody').first.css('div.desc a').text 
-    # epa class doc.css('tbody').first.css('th.vclass').text
-    # mpg doc.css('tbody').first.css('td.auto-mpg').text
+    car_array = doc.css('table#bestcars').css('tbody')
+    
+    car_array.each do |car_row|
+    car = {
+      model:  car_row.css('div.desc a').text
+      epa_class: car_row.css('th.vclass').text
+      mpg: car_row.css('td.auto-mpg').text
+      fuel_type: car_row.css('button').children.text
+      url:
+    }
+    
+    
+    binding.pry
+    end
+    
   # 1 first level to get car name, type of engine, gas mileage
   # collect array of hashes. each hash is a car.  the hash will be used to create instances of the car class
   end
