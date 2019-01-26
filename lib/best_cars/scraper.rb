d2fc55d1560ce8a1c224d61b438591e52e35b19b
Marie Burns <mcburns2222@gmail.com>
2019-01-26 01:52:06 +0000

    Fix second level scrape method

--- lib/best_cars/scraper.rb
+++ lib/best_cars/scraper.rb
@@ -22,15 +22,15 @@ class BestCars::Scraper
          #the 2 BMW cars are returning on the same line.
   
       car = BestCars::Car.new(car_attributes)
-      binding.pry
       end
   end
   
   def self.scrape_range_cost(selected_car_url)
-    doc = Nokogiri::HTML(open)
-    puts "#{selected_car_url}"
-  # 1 second level to get range and emissions and other data
-  # called based on user input. takes an argument of the car URL.
+    doc = Nokogiri::HTML(open(selected_car_url))
+    puts "\n"
+    puts "The total range is #{doc.css("div.yui-content div#tab1 table tr td.sbsCellData div.rangeGraphic div.phevRange").text.delete(" miles Total Range")} miles."
+    puts "\n"
+    puts "The annual fuel cost for both electricity and gasoline is #{doc.css("div.yui-content div#tab1 table tr td.sbsEconData")[0].text.delete("Electricity + Gasoline: ")}."
   end
-  
+    
 end
